e3899eda5210905a68adc38d856c5fc8a048b6b1
keegano86 <keeganoloughlin@gmail.com>
2025-12-17 14:22:46 -0600

    Setting up this file

--- arrays.structs.sol
+++ arrays.structs.sol
@@ -5,4 +5,10 @@ contract SimpleStorage {
     // favoriteNumber gets initialized to 0 if no value
 
     uint256 public favoriteNumber; //0
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+
 }
