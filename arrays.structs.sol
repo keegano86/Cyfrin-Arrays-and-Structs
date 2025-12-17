273af8c61c815dda8fdb8f39aa9020481e82f104
keegano86 <keeganoloughlin@gmail.com>
2025-12-17 14:03:43 -0600

    Setting up this file

--- arrays.structs.sol
+++ arrays.structs.sol
@@ -1,2 +1,8 @@
-// SPDX-License-identifier: MIT
-pragma Solidity 0.8.31; /solidity versions
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.31; //solidity versions
+
+contract SimpleStorage {
+    // favoriteNumber gets initialized to 0 if no value
+
+    uint256 public favoriteNumber; //0
+}
