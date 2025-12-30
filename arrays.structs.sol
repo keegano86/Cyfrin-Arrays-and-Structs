d21456cee9b50e37fc551177a907545ff138eba0
keegano86 <keeganoloughlin@gmail.com>
2025-12-30 14:16:05 -0600

    updated pragma

--- arrays.structs.sol
+++ arrays.structs.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.31; //solidity versions
+pragma solidity 0.8.24; //solidity versions
 
 contract SimpleStorage {
     // favoriteNumber gets initialized to 0 if no value
